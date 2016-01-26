2f7973a0777b14e7481b340b26d819a924ec18b4
zvory <livingalex@gmail.com>
2016-01-26 15:27:15 -0500

    code/package cleanup and logging added

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 ###Nodejs Price Server
-WebM recording of visualization here: [zvoryg.in/graph.webm](http://zvoryg.in/graph.webm)
+Live Visualization (assuming server is up) [can be found here.](http://zvoryg.in/viz/)
 
-Live Visualization (assuming server is up) [can be found here.](http://zvoryg.in/nodeServer/)
-
-__server.js__ Updates a price variable based on time. On update, it broadcasts using sockets the new price.
+__server.js__ Updates a price variable based on time. On update, it broadcasts using sockets the new price. Includes logging funcionality.
 
 __dash.html__ Contains the cool live visualization. Real-time updating chart of price so far, including 30-day Moving Average. Uses smoothie.js for visualization.
-
-__client.js__ Gets prices from server using TCP.
 
 __smoothie.js__ JS Charting library from [http://smoothiecharts.org/](http://smoothiecharts.org/)
 
--- server.js
+++ server.js
@@ -1,48 +1,40 @@
-var net = require('net');
 var io = require('socket.io').listen(9091);
+var fs = require('fs');
 
+//Our Details
 var HOST = 'csclub.uwaterloo.ca';
 var PORT = 9090;
 
-var price =100;
+var price = 100;
 
-//var microPrice= 100;
-
-//var macroPrice = 100;
-//var lastThirtyMacroPrice = 100;
-
+//**********************************************
+//*****     SOCKETS TO DASHBOARD
+//**********************************************
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
-    
-    // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    
-    // Add a 'data' event handler to this instance of socket
-    sock.on('data', function(data) {
-        sock.write("price " + price);
-    });
-
-    // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-    });
-    
-}).listen(PORT, HOST);
 
 io.on('connection', function(socket){
-    console.log("connection: " + socket);
+    console.log(new Date() + ": new connection");
+
+fs.appendFile('log.txt', new Date() + ": new connection \n", function (err) {
+    if (err) return console.log(err);
+});
 });
 
-
-
+//**********************************************
+//*****     PRICE CODE
+//**********************************************
 function updatePrice(){
     //microPrice = microPrice + Math.ciel (Math.random() * 10) - 5 + (100-microPrice) *0.1;
     //macroPrice = macroPrice + Math.ciel (Math.random() * 2) - 1 + (100-macroPrice) *0.01;
     price += Math.random() - 0.5 + (100-price) * 0.001;
     io.sockets.emit('price', price);
 }
-
 setInterval(updatePrice, 50);
+
+
+//**********************************************
+//*****     INITIALIZATION 
+//**********************************************
 console.log('Server listening on ' + HOST +':'+ PORT);
